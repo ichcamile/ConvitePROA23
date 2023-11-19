364442ee22ee68a89704c51bb95f93872360d2d7
ichcamile <camilesantana21@gmail.com>
2023-11-19 16:15:53 -0300

    add: logos

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      375: {
+      slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+        },
 
-  var appendNumber = 4;
-  var prependNumber = 1;
-  document
-    .querySelector(".prepend-2-slides")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.prependSlide([
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
-      ]);
-    });
-  document
-    .querySelector(".prepend-slide")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.prependSlide(
-        '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
-      );
-    });
-  document
-    .querySelector(".append-slide")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.appendSlide(
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
-      );
-    });
-  document
-    .querySelector(".append-2-slides")
-    .addEventListener("click", function (e) {
-      e.preventDefault();
-      swiper.appendSlide([
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-        '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
-      ]);
-    });
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+  .querySelector(".prepend-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  });
+document
+  .querySelector(".prepend-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide(
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  });
